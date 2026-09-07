71fded90bf23aa82a8536e3bca6f551c2b6d8ea5
Benjamin Batres <benbatres1212@gmail.com>
2026-09-06 22:06:49 -0700

    fixed: implemented close to mobile nav links

--- app/components/ui/Links.tsx
+++ app/components/ui/Links.tsx
@@ -1,14 +1,15 @@
+'use client'
 import Link from "next/link";
-
 interface TLink {
   path: string;
   name: string;
+  setIsOpen?(boolean: boolean): void;
 }
 
-export default function Links({ path, name }: TLink) {
+export default function Links({ path, name, setIsOpen }: TLink) {
   return (
     <li>
-      <Link href={path} className="font-medium">
+      <Link href={path} className="font-medium" onClick={() => setIsOpen?.(false)}>
         {name}
       </Link>
     </li>
