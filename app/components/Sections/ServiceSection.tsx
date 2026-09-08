4e4aca5a100e343b84f59890c6d7e8d1402a23d9
Benjamin Batres <benbatres1212@gmail.com>
2026-09-07 19:08:24 -0700

    feature: implemented service deatil component

--- app/components/Sections/ServiceSection.tsx
+++ app/components/Sections/ServiceSection.tsx
@@ -3,6 +3,7 @@ import { IoIosGlobe } from "react-icons/io";
 import { RiTargetFill } from "react-icons/ri";
 import { GrPowerCycle } from "react-icons/gr";
 import { RiToolsFill } from "react-icons/ri";
+import ServiceDetails from "../ui/ServiceDetails";
 
 export default function ServiceSection() {
   const services = [
@@ -40,17 +41,7 @@ export default function ServiceSection() {
         />
         <div className="grid md:grid-cols-2 gap-6">
           {services.map((service, id) => (
-            <div key={id} className="bg-white p-6 rounded-lg space-y-6">
-              <div className="size-20 bg-[#ebebeb] rounded-full flex justify-center items-center">
-                <div className="text-4xl">{service.icon}</div>
-              </div>
-              <div className="space-y-3">
-                <h3 className="text-[25px]/[120%] font-bold">
-                  {service.title}
-                </h3>
-                <p>{service.description}</p>
-              </div>
-            </div>
+            <ServiceDetails key={id} title={service.title} description={service.description} icon={service.icon}/>
           ))}
         </div>
       </div>
