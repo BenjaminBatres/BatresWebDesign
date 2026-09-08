0c92dca69fb323deadcd85941719ddfab2b523c0
Benjamin Batres <benbatres1212@gmail.com>
2026-09-07 19:12:57 -0700

    feature: implemented why work with me section

--- app/components/Sections/WhyWorkWithMeSection.tsx
+++ app/components/Sections/WhyWorkWithMeSection.tsx
@@ -4,32 +4,30 @@ import { PiStarFour } from "react-icons/pi";
 import ServiceDetails from "../ui/ServiceDetails";
 
 export default function WhyWorkWithMeSection() {
-    const features = [
-        {
-          icon: <FaRegHandshake />,
-          title: "Personal Service ",
-          description:
-            "Work directly with the person building your website.",
-        },
-        {
-          icon: <FaDollarSign className="text-green-500" />,
-          title: "Affordable Pricing",
-          description:
-            "Professional websites without the agency price",
-        },
-        {
-          icon: <PiStarFour className="text-violet-500" />,
-          title: "Modern Design",
-          description:
-            "Clean, responsive websites built to make a strong first impression.",
-        },
-        {
-          icon: <GoGear  className="text-[#6e6c7f]" />,
-          title: "Technical Support",
-          description:
-            "I handle the technical sidd so you can focus on your business.",
-        },
-      ];
+  const features = [
+    {
+      icon: <FaRegHandshake />,
+      title: "Personal Service ",
+      description: "Work directly with the person building your website.",
+    },
+    {
+      icon: <FaDollarSign className="text-green-500" />,
+      title: "Affordable Pricing",
+      description: "Professional websites without the agency price",
+    },
+    {
+      icon: <PiStarFour className="text-violet-500" />,
+      title: "Modern Design",
+      description:
+        "Clean, responsive websites built to make a strong first impression.",
+    },
+    {
+      icon: <GoGear className="text-[#6e6c7f]" />,
+      title: "Technical Support",
+      description:
+        "I handle the technical sidd so you can focus on your business.",
+    },
+  ];
   return (
     <section className="py-16 md:py-20 px-6">
       <div className="max-w-300 mx-auto space-y-12 md:space-y-16">
@@ -41,9 +39,14 @@ export default function WhyWorkWithMeSection() {
           agency-sized price tag.
         </p>
         <div className="grid sm:grid-cols-2 xl:grid-cols-4 gap-6">
-            {features.map((feature, id) => (
-                <ServiceDetails key={id} title={feature.title} description={feature.description} icon={feature.icon}/>
-            ))}
+          {features.map((feature, id) => (
+            <ServiceDetails
+              key={id}
+              title={feature.title}
+              description={feature.description}
+              icon={feature.icon}
+            />
+          ))}
         </div>
       </div>
     </section>
