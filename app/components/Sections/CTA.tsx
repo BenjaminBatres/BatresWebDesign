1359024f6a7e81dc042fa58bccb4f8c532e9d4fd
Benjamin Batres <benbatres1212@gmail.com>
2026-09-09 09:27:18 -0700

    fix: changed text size

--- app/components/Sections/CTA.tsx
+++ app/components/Sections/CTA.tsx
@@ -6,10 +6,10 @@ export default function CTA() {
       <div className="space-y-6">
         <div className="space-y-8">
           <div className="flex flex-col items-center gap-2">
-            <h3 className="text-white font-semibold text-xl">Get Started</h3>
+            <h3 className="text-white font-semibold md:text-xl">Get Started</h3>
             <div className="w-6 h-0.5 bg-white" />
           </div>
-          <h2 className="text-[39px] md:text-[49px] leading-[120%] tracking-[-0.5px] text-white font-bold text-center max-w-200">
+          <h2 className="text-[39px] md:text-[49px] leading-[120%] tracking-[-0.5px] text-white font-semibold text-center max-w-200">
             Ready to bring your vision to life? Get in touch today!
           </h2>
         </div>
