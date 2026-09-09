7d88e3ab4ae802cfbc25e93fbffdac61e12342ed
Benjamin Batres <benbatres1212@gmail.com>
2026-09-08 20:52:13 -0700

    feature: implemeneted footer section

---
+++ app/components/Sections/CTA.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+export default function CTA() {
+  return (
+    <div className="p-6 md:p-10 bg-[#151424] rounded-2xl flex flex-col items-center gap-8">
+      <div className="space-y-6">
+        <div className="space-y-8">
+          <div className="flex flex-col items-center gap-2">
+            <h3 className="text-white font-semibold text-xl">Get Started</h3>
+            <div className="w-6 h-0.5 bg-white" />
+          </div>
+          <h2 className="text-[39px] md:text-[49px] leading-[120%] tracking-[-0.5px] text-white font-bold text-center max-w-200">
+            Ready to bring your vision to life? Get in touch today!
+          </h2>
+        </div>
+        <p className="text-white! text-center md:text-lg">
+          Let's build a website your business can be proud of.
+        </p>
+      </div>
+      <Link
+        href={"/contact"}
+        className="bg-white rounded-full px-6 py-3 text-[15px] tracking-[-0.5px] font-semibold"
+      >
+        Get in touch
+      </Link>
+    </div>
+  );
+}
---
+++ app/components/Sections/Footer.tsx
@@ -0,0 +1,61 @@
+import Link from "next/link";
+import CTA from "./CTA";
+import Links from "../ui/Links";
+
+export default function Footer() {
+  const links = [
+    {
+      path: "/",
+      name: "Home",
+    },
+    {
+      path: "#projects",
+      name: "Projects",
+    },
+    {
+      path: "#services",
+      name: "Services",
+    },
+    {
+      path: "#process",
+      name: "Process",
+    },
+    {
+      path: "#pricing",
+      name: "Pricing",
+    },
+    {
+      path: "#faq",
+      name: "FAQ",
+    },
+  ];
+  return (
+    <footer className="pt-16 md:pt-40 pb-6 bg-[#0e0e11] px-6">
+      <div className="max-w-300 mx-auto space-y-16 lg:space-y-20">
+        <CTA />
+        <div className="flex flex-col lg:flex-row gap-8 justify-between ">
+          <div className="space-y-4">
+            <h2 className="text-white/90 text-xl font-semibold">
+              BatresWebDesign
+            </h2>
+            <p className="text-white/90! text-sm max-w-sm">
+              Let's build a modern website that helps your business stand out
+              and makes it easier for customers to reach you.
+            </p>
+          </div>
+          <div className="space-y-4">
+            <h3 className="text-white text-xl font-semibold">Links</h3>
+            <ul className="flex flex-col md:flex-row gap-3 text-white/90">
+              {links.map((link, id) => (
+                <Links key={id} name={link.name} path={link.path} />
+              ))}
+            </ul>
+          </div>
+        </div>
+        <p className="text-white/90! text-sm">
+          © {new Date().getFullYear()} BatresWebDesign. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
