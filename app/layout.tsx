04cc522a9fd80cb4e1e3d3909ba9829240fb11e0
Benjamin Batres <benbatres1212@gmail.com>
2026-09-09 16:01:20 -0700

    fix: fixed open graph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
     siteName: "Batres Web Design",
     images: [
       {
-        url: "./assets/OpenGraphImg",
+        url: "/assets/OpenGraphImg.png",
         width: 1200,
         height: 630,
         alt: "Batres Web Design",
