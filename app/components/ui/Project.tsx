92f51877115de6b1912f9542a1997121164abfff
Benjamin Batres <benbatres1212@gmail.com>
2026-09-18 14:30:36 -0700

    feat: implmented calle heat project

--- app/components/ui/Project.tsx
+++ app/components/ui/Project.tsx
@@ -5,9 +5,16 @@ interface TProject {
   image: string | StaticImageData;
   title: string;
   tag: string;
+  description: string;
 }
 
-export default function Project({ link, image, title, tag }: TProject) {
+export default function Project({
+  link,
+  image,
+  title,
+  tag,
+  description,
+}: TProject) {
   return (
     <motion.a
       initial={{ opacity: 0, y: 75 }}
@@ -26,11 +33,14 @@ export default function Project({ link, image, title, tag }: TProject) {
           className="hover:scale-105 duration-500"
         />
       </figure>
-      <div className="px-4 pb-4 flex flex-col lg:flex-row gap-4 lg:gap-0 items-center justify-between">
-        <h3 className="text-[25px] font-bold">{title}</h3>
-        <div className="bg-[#ebebeb] text-sm px-4 py-1.5 rounded-full font-semibold">
-          {tag}
+      <div className="px-4 pb-4 space-y-3">
+        <div className="flex flex-col lg:flex-row gap-4 lg:gap-0 items-center justify-between">
+          <h3 className="text-[25px] font-bold">{title}</h3>
+          <div className="bg-[#ebebeb] text-sm px-4 py-1.5 rounded-full font-semibold">
+            {tag}
+          </div>
         </div>
+        <p className="text-sm text-center md:text-left">{description}</p>
       </div>
     </motion.a>
   );
